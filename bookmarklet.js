745bdbbadd41d4f069560ef1492fb900176b2cb7
tmikolaj <mikolajt55@gmail.com>
2025-05-08 16:39:17 +0200

    Subtle disorientation added

--- bookmarklet.js
+++ bookmarklet.js
@@ -2,6 +2,8 @@ javascript:(function() {
     const doesStyleExist = document.body.classList.contains('cDarkMode');
     document.body.classList.toggle('cDarkMode', !doesStyleExist);
 
+    let chance = Math.floor(Math.random() * 5);
+
     const css = `
         html, body {
             margin: 0;
@@ -23,4 +25,11 @@ javascript:(function() {
     const style = document.createElement('style');
     style.textContent = css;
     document.head.appendChild(style);
+    if (chance === 0) {
+        document.body.style.transform = 'rotate(2deg)';
+        document.body.style.transition = 'transform 0.4s ease';
+    } else {
+        document.body.style.transform = 'rotate(0deg)';
+        document.body.style.transition = 'transform 0.4s ease';
+    }
 })();
